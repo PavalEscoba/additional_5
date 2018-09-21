79a3b469717cdd4d68c0f421c2798806c61bef58
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-21 16:55:57 +0300

    Промудохался целый день. Не все тесты. Вернусь

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   //i'm not that smart. but i googled https://www.martinkysel.com/codility-brackets-solution/
-  var string = '|()|(||)||';
-  var bracketsCon= [['(', ')'], ['|', '|']];
-  check('|()|(||)||', [['(', ')'], ['|', '|']]);
-  function check(str, bracketsConfig){
-    if(str.length%2 !=0){
-      return false;
-    }
-    const stack = [];
-    for(let i = 0; i < str.length; i++){
-      for(let j = 0; j<bracketsConfig.length; j++){
-        if(str[i]==bracketsConfig[j][0]){
-          console.log("пушіцца номер: " + i +" а именно "+ str[i]);
-          stack.push(str[i]);
-          if(str[i] == '|' || str[i]=='7' || str[i]=='8'){
-            if(stack.indexOf(str[i]) != -1 ){
-              stack.pop()
-            }
-            else if(stack.indexOf(str[i]) != -1 ) // проверить на совпадение предыдущей дужки из стека. если не совпадает вернуть false
-          }
-        }
-        else if(str[i] == bracketsConfig[j][1]){
-          console.log("удаляется номер: " + i +" а именно "+ str[i]);
-          stack.pop();
-        }
+  //it doesn't pass all tests. i'll come back later. hope so
+  if(str.length%2 !=0){
+    return false;
+  };
+  if(str.length==0){
+    return false;      
+  }
+  const stack = [];
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(str[i]==bracketsConfig[j][0] && stack[i-1]!=bracketsConfig[j][1]){
+        stack.push(str[i]);
+      }
+      else if (str[i] == bracketsConfig[j][1] || stack.indexOf(str[i], stack[i-1])==1){
+        stack.pop();
       }
     }
-    console.log(stack);
-  }
+  } 
+  return stack.length ? false : true;
 }
