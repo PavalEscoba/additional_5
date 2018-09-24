590eb57e69694e4d37df65aee02391c120b3d0c3
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-24 15:42:07 +0300

    исправлено

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  //i'm not that smart. but i googled https://www.martinkysel.com/codility-brackets-solution/
-  //it doesn't pass all tests. i'll come back later. hope so
   if(str.length%2 !=0){
     return false;
   };
   if(str.length==0){
     return false;      
   }
-  const stack = [];
+  let stack = [];
   for(let i = 0; i < str.length; i++){
     for(let j = 0; j < bracketsConfig.length; j++){
-      if(str[i]==bracketsConfig[j][0] && stack[i-1]!=bracketsConfig[j][1]){
+      var last = stack[stack.length-1];
+           
+      if(str[i]==bracketsConfig[j][1] && stack.length != 0 && last == bracketsConfig[j][0]){
+        stack.pop(); 
+      }
+      else if (str[i] == bracketsConfig[j][0]){
+       
         stack.push(str[i]);
       }
-      else if (str[i] == bracketsConfig[j][1] || stack.indexOf(str[i], stack[i-1])==1){
-        stack.pop();
-      }
     }
+  
   } 
+  
   return stack.length ? false : true;
 }
